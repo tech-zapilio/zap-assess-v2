3c0508a9a74b0ff43359594f1d9ac5e349a2b2ce
Yogesh Sharma <yogeshsharma@Yogeshs-MacBook-Air.local>
2024-01-29 16:38:58 +0530

    Route prefix

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 export default defineConfig({
   build: {
-    outDir: "dist",
+    outDir: "build",
   },
   server: {
     host: true,
